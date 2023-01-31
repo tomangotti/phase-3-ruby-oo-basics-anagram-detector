7b7a11f6719b7a503c5d32f1feff084d7adbfe8e
thomas <tom.angotti11@gmail.com>
2023-01-30 17:13:29 -0700

    commit tree e2599e9d1269cfab7363059923ac4e875194a826

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(phrase)
+        phrase.select {|x| x.split("").sort == @word.split("").sort}
+
+    end
+end
+
